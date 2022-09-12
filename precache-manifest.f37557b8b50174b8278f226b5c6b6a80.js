28fbf593013987ee20461a9fbb002c5e120a9ba7
Soumyadeep <soumyadeeproy.csbs@gmail.com>
2022-09-12 14:30:36 +0530

    Updates

---
+++ precache-manifest.f37557b8b50174b8278f226b5c6b6a80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "44361d81e1601f43b515",
+    "url": "/portfolio/static/css/main.7c7ca1a3.chunk.css"
+  },
+  {
+    "revision": "44361d81e1601f43b515",
+    "url": "/portfolio/static/js/main.d95c6939.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "54fc7d34ca3f9575c5f6",
+    "url": "/portfolio/static/js/2.7722f374.chunk.js"
+  },
+  {
+    "revision": "77b16ff47dcbfa25c46e4a23ccf41095",
+    "url": "/portfolio/static/media/quinton-coetzee-gpa8Y_Fk7Rg-unsplash.77b16ff4.jpg"
+  },
+  {
+    "revision": "e70f8d94300ef955886d51582553e93f",
+    "url": "/portfolio/static/media/vackground-SfDofjXtxHE-unsplash.e70f8d94.jpg"
+  },
+  {
+    "revision": "b9572c20bd21072a7e4a0276c5868f75",
+    "url": "/portfolio/static/media/myimage.b9572c20.jpeg"
+  },
+  {
+    "revision": "8d8ebe3e4b26f1a0581b94b30aa8b849",
+    "url": "/portfolio/index.html"
+  }
+];
